7fdca1454744da1254a622103cf35984c23bf8f5
nishchay trivedi <nishchay.trivedi@theinternetfolks.com>
2021-04-11 00:06:40 +0530

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api/user", usersRouter);
